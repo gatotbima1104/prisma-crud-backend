db5306e7b85dcd3ec2fc3cc4eb0339f67773f2e1
gatotbima1104 <gatotbima2002@gmail.com>
2023-05-04 02:07:50 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
 const productController = require("./product/product.controller")
 
 dotenv.config();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -12,7 +15,7 @@ const PORT = process.env.PORT;
 app.use("/product", productController)
 
 app.listen(PORT, () =>{
-    console.log("server is running on port 3000")
+    console.log("server is running on port 5000")
 })
 
 

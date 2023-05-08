9b0836ba1f0b788a5b56e95c775b20c2c41afe92
gatotbima1104 <gatotbima2002@gmail.com>
2023-05-08 21:24:42 +0700

    fix the connection db

--- src/event/event.service.js
+++ src/event/event.service.js
@@ -1,11 +1,6 @@
 // handle bisnis logic
 // dipisah agar tenggung jawabnya terisolate dan fungsionalnya reusable
 
-// const prisma = require("../db");
-// const prisma = require("../db");
-// import * as bcrypt from 'bcrypt';
-// const bcrypt = require("bcrypt");
-// import { bcrypt } from bcryptjs;
 const { findevent, findeventById, insertevent, removeeventById, edtieventById, createUser  } = require("./event.repository");
 
 // const getUsername = async (username) => {
